ea2a44129a2912c469cd6036595aea470b40b2f5
Gabriel <132038804+Zacksyg@users.noreply.github.com>
2023-05-31 03:05:21 -0300

    Update animal.py

--- P2/animal.py
+++ P2/animal.py
@@ -10,31 +10,4 @@ class Animal:
         print("Espécie:", self.especie)
 
 
-class CadastroAnimais:
-    def __init__(self):
-        self.animais = []
 
-    def adicionar_animal(self, animal):
-        self.animais.append(animal)
-
-    def exibir_animais(self):
-        if not self.animais:
-            print("Nenhum animal cadastrado.")
-        else:
-            for animal in self.animais:
-                animal.exibir_detalhes()
-
-
-
-cadastro = CadastroAnimais()
-
-animal1 = Animal("Rex", 5, "Cachorro")
-cadastro.adicionar_animal(animal1)
-
-animal2 = Animal("Mia", 3, "Gato")
-cadastro.adicionar_animal(animal2)
-
-animal3 = Animal("Daisy", 2, "Coelho")
-cadastro.adicionar_animal(animal3)
-
-cadastro.exibir_animais()
