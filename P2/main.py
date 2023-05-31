f39c7064635401b1624324513ffda7b866f77684
Gabriel <132038804+Zacksyg@users.noreply.github.com>
2023-05-31 03:04:57 -0300

    Update main.py

--- P2/main.py
+++ P2/main.py
@@ -1,1 +1,12 @@
+from animal import Animal
+from cadastro_animal import CadastroAnimais
 
+if __name__== '__main__':
+
+
+    cadastro = CadastroAnimais()
+
+    animal1 = Animal('Zé' , 12, 'felino' , 'grande', 'nenhuma')
+    cadastro.adicionar_animal(animal1)
+
+    cadastro.exibir_animais()
