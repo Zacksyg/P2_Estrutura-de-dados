ea7514d2c87acc471de6aebb385a92aae3f1e33d
Gabriel <132038804+Zacksyg@users.noreply.github.com>
2023-06-07 04:08:29 -0300

    Update telas.py

--- P2/telas.py
+++ P2/telas.py
@@ -1,6 +1,32 @@
 from customtkinter import *
 
 
+class Tela_cadastro_animal:
+    def __init__(self):
+        self.root_animal = CTk()
+        self.widgets()
+        self.root_animal.mainloop()
+        
+
+    def widgets(self):
+        self.root_animal.title('Tela de Cadastro')
+        self.root_animal.geometry('700x700')
+        self.root_animal.resizable(False, False)
+        self.root_animal.config(background='#B0E0E6')
+
+        self.texto_inicial = CTkLabel(master=self.root_animal, text='Digite ou Selecione Abaixo As Informações Do Animal.', fg_color='#B0E0E6', text_color='#2F4F4F', corner_radius=0,
+                                      font=('Arial',20)).place(relx= 0.20, rely=0.05)
+        
+        self.text_especie = CTkLabel(master=self.root_animal,text='Especie', fg_color='#B0E0E6', text_color='#2F4F4F', corner_radius=0,
+                                      font=('Arial',20)).place(relx= 0.45, rely=0.12)
+        
+        self.box_especie = CTkComboBox(master=self.root_animal, values=('Canino', 'Felino', 'Reptil', 'Peixe', 'Ave'), width=300, font=('Arial', 20)).place(relx= 0.30, rely= 0.17)
+
+
+
+
+
+
 class Menu(Tela_cadastro_animal):
     def __init__(self):
         self.root_menu = CTk()
